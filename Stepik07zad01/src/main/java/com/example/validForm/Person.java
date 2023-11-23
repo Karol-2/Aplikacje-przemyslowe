1b714ffc83bb8edd089b3826bf12e3ea1c55ac97
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-23 21:31:17 +0100

    Stepik07/01

--- Stepik07zad01/src/main/java/com/example/validForm/Person.java
+++ Stepik07zad01/src/main/java/com/example/validForm/Person.java
@@ -1,11 +1,9 @@
 package com.example.validForm;
 
 import lombok.Data;
+import lombok.Value;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 public class Person {
@@ -17,16 +15,15 @@ public class Person {
     @Min(value = 0, message = "Age must be at least zero")
     private int age;
 
-    @NotNull(message = "Post code is required")
-    @Size(min = 2, message = "Post code should be in format XX-XXX") //TODO: postcode
-    private String postCode;
+    @NotNull(message = "Postal code is required")
+    @Pattern(regexp = "\\d\\d-\\d\\d\\d", message = "Post code should be in format XX-XXX")
+    private String postalCode;
 
     @NotNull(message = "Salary is required")
     @Min(value= 2000,message = "Salary should be at least 2000")
-    @Max(value= 3000,message = "Salary should be max 3000")
+    @Max(value= 3000,message = "Salary should be at most 3000")
     private long salary;
 
-    @NotNull(message = "You need to accept user condictions")
-    @Size( message = "Name should be start at least two characters") //TODO: postcode
-    private boolean acceptedCondictionw;
+    @NotNull(message = "Conditions must be accepted")
+    private boolean acceptedCondictions;
 }
